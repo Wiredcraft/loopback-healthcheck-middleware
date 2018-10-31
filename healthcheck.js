2e44dc6282ee02122c8aef39b482e510f3869399
KateWu <woodpig07@gmail.com>
2018-10-31 16:17:53 +0800

    basic lib

---
+++ healthcheck.js
@@ -0,0 +1,35 @@
+'use strict';
+
+const path = require('path');
+const defautOpts = {
+  path: '/health',
+  versionFile: '../../../package.json'   // path relative to healthcheck.js
+};
+
+module.exports = function(options) {
+  options = options ? Object.assign(defautOpts, options) : defautOpts;
+  const started = new Date();
+  const verpath = path.join(__dirname, options.versionFile);
+  let version = 'unknown';
+  try {
+    const ver = require(verpath);
+    version = ver && ver.version || version;
+  } catch (e) {
+    if (e.code !== 'MODULE_NOT_FOUND') {
+      throw e;
+    }
+    console.log('Version file not found: ', verpath);
+  }
+  
+  return function healthcheck(req, res, next) {
+    if (req.path === options.path) {
+      res.send({
+        started: started,
+        uptime: (Date.now() - Number(started)) / 1000,
+        version
+      });
+    } else {
+      next();
+    }
+  };
+};
